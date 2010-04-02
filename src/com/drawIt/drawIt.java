572aac05b9f03c1a00718c6a302d95f083517925
eworldisnotenuff <eworldisnotenuff@0bbe4287-2352-73a6-adab-d028de2c3b49>
2010-04-02 14:28:31 +0000

    tested detecting submit event and also auto submitting

--- src/com/drawIt/drawIt.java
+++ src/com/drawIt/drawIt.java
@@ -4,14 +4,15 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
 import android.webkit.*;
 
 //yiling here just testing
 public class drawIt extends Activity {
+	Handler handler = new Handler();
 	final Context myApp = this;
 	WebView webview;
 	/** Called when the activity is first created. */
-	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
@@ -20,7 +21,7 @@ public class drawIt extends Activity {
 		webview.getSettings().setJavaScriptEnabled(true);
 		
 		webview.addJavascriptInterface(new MyJavaScriptInterface(), "HTMLOUT");
-		webview.loadUrl("http://www.google.com/");
+		webview.loadUrl("http://www.gmail.com/");
 		
 	}
 	private class HelloWebViewClient extends WebViewClient {
@@ -28,8 +29,16 @@ public class drawIt extends Activity {
 		public void onPageFinished(WebView view, String url)
 	    {
 	        /* This call inject JavaScript into the page which just finished loading. */
-			webview.loadUrl("javascript:document.getElementsByName('q')[0].onfocus = " + "function() { window.HTMLOUT.showHTML('textfield focused'); };");
-			//webview.loadUrl("javascript:window.HTMLOUT.showHTML(document.getElementsByName('q')[0]);");
+			
+			
+			webview.loadUrl("javascript:(function() { document.getElementsByName('Email')[0].onfocus = " 
+					+ "function() { window.HTMLOUT.showHTML('textfield focused'); }; })()");
+			
+			//webview.loadUrl("javascript:function wave() {document.getElementsByName('Email')[0].value = 'cs3248.g4@gmail.com'; }");
+			
+			
+			//uncomment this line to autofill the login details when pressing submit
+			//webview.loadUrl("javascript:document.forms['gaia_loginform'].onsubmit = function() {document.getElementsByName('Email')[0].value = 'cs3248.g4@gmail.com'; document.getElementsByName('Passwd')[0].value = 'ilovecs3248';};");
 	    }
 	}
 	
@@ -40,13 +49,23 @@ public class drawIt extends Activity {
 	    @SuppressWarnings("unused")
 	    public void showHTML(String html)
 	    {
-	        new AlertDialog.Builder(myApp)
+	        /*new AlertDialog.Builder(myApp)
 	            .setTitle("HTML")
 	            .setMessage(html)
 	            .setPositiveButton(android.R.string.ok, null)
 	        .setCancelable(false)
 	        .create()
-	        .show();
+	        .show();*/
+	        
+	        handler.post(new Runnable() {
+                public void run() {
+                	webview.loadUrl("javascript:document.getElementsByName('Email')[0].value = 'cs3248.g4@gmail.com';");
+                	webview.loadUrl("javascript:document.getElementsByName('Passwd')[0].value = 'ilovecs3248';");
+                	
+                	//uncomment this line to auto-submit
+                	//webview.loadUrl("javascript:document.forms['gaia_loginform'].submit();");
+                }
+            });
 	    }
 	}
 }

9111be5842683fbe59a3876134a67cc6cef5b74f
eworldisnotenuff <eworldisnotenuff@0bbe4287-2352-73a6-adab-d028de2c3b49>
2010-04-06 16:07:39 +0000

    changed the way lines are drawn

--- trunk/src/com/drawIt/DrawView.java
+++ trunk/src/com/drawIt/DrawView.java
@@ -4,8 +4,10 @@ import java.util.ArrayList;
 import java.util.Iterator;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -13,6 +15,10 @@ public class DrawView extends View {
 	
 	Paint paint;
 	ArrayList<Line> lineList;
+	
+	Bitmap b;
+	Canvas c;
+	Rect bitmapRect;
 
 	public DrawView(Context context) {
 		super(context);
@@ -30,21 +36,38 @@ public class DrawView extends View {
 	}
 
 	protected void onDraw(Canvas canvas) {
+		if(b != null)
+			canvas.drawBitmap(b, bitmapRect,bitmapRect,  null);
+		/*
 		Iterator<Line> iterator = lineList.iterator();
 		while(iterator.hasNext()) {
 			Line line = iterator.next();
 			canvas.drawLine(line.startX, line.startY, line.stopX, line.stopY, paint);
-		}
+		}*/
 	}
 
 	public void addLine(float startX, float startY, float stopX, float stopY) {
-		lineList.add(new Line(startX, startY, stopX, stopY));
+		//lineList.add(new Line(startX, startY, stopX, stopY));
 
+		if(b == null) {
+			b = Bitmap.createBitmap(getWidth(), getHeight(), Bitmap.Config.ARGB_8888);
+			c = new Canvas();
+			c.setBitmap(b);
+			bitmapRect = new Rect(0,0,b.getWidth(),b.getHeight());
+		}
+		
+		c.drawLine(startX, startY, stopX, stopY, paint);
+		
+		
 		this.invalidate();
 	}
 	
 	public void clearLines() {
-		lineList.clear();
+		//lineList.clear();
+		
+		b.recycle();
+		b = Bitmap.createBitmap(getWidth(), getHeight(), Bitmap.Config.ARGB_8888);
+		c.setBitmap(b);
 		
 		this.invalidate();
 	}

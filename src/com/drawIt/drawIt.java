376776ee6a37bc89e98af84fcb74c59702b2fc2b
eworldisnotenuff <eworldisnotenuff@0bbe4287-2352-73a6-adab-d028de2c3b49>
2010-04-08 09:12:58 +0000

    - extended login detection to other websites. currently tested with facebook, gmail and hotmail - domain detected as the words after :// and before /. for eg. http://www.facebook.com/abc.html --> domain = www.facebook.com - due to a complication with javascript onsubmit event, pass stroke saving is triggered by the password field losing focus instead of pressing the submit button - pass login screen will only appear if the user has:   1. previously saved a pass stroke for this domain   2. click on the userid field (not password field)   3. at least the userid field or the password field must be empty

--- src/com/drawIt/drawIt.java
+++ src/com/drawIt/drawIt.java
@@ -1,20 +1,46 @@
 package com.drawIt;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Window;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 
 public class drawIt extends Activity {
 
+	static Context context;
+	static Activity activity;
 	WebView webview;	
 	
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		context = this;
+		activity = this;
+		
+		getWindow().requestFeature(Window.FEATURE_PROGRESS);
 		setContentView(R.layout.main);
+		setProgressBarVisibility(true);
 		
 		webview = (WebView) findViewById(R.id.appView);
 		webview.setWebViewClient(new JSWebViewClient());
+		webview.setWebChromeClient(new WebChromeClient() {
+			public boolean onJsAlert(WebView view, String url, String message, final android.webkit.JsResult result) {  
+				System.out.println(message);		  
+		        return false;  
+		    }; 
+		    
+		    public void onProgressChanged(WebView view, int progress) {System.out.println(progress);
+		        activity.setProgress(progress * 1000);
+		      }
+		});
+		
+		
+		webview.getSettings().setSavePassword(false); //disable webview default password saving
 		webview.getSettings().setJavaScriptEnabled(true);
 		
 		webview.addJavascriptInterface(new JSCallback(this), "JSCALLBACK");
@@ -92,15 +118,19 @@ public class drawIt extends Activity {
 		//called when the user manually logins for the first to a domain
 		//show the draw pass stroke interface which will save the pass stroke
 		
-		Intent intent = new Intent(this, DrawScreen.class);
-    	intent.putExtra("mode", DrawScreen.DRAW_TO_SAVE);
-    	intent.putExtra("domain", domain);
-    	intent.putExtra("formName", formName);
-    	intent.putExtra("useridField", useridField);
-    	intent.putExtra("userid", userid);
-    	intent.putExtra("passwdField", passwdField);
-    	intent.putExtra("passwd", passwd);
+		Util.pl(formName + " " + useridField + " " + userid + " " + passwdField + " " + passwd);
 		
-		startActivityForResult(intent, DrawScreen.DRAW_TO_SAVE);
+		if(DatabaseManager.hasPassStroke(domain, userid, passwd) == false) {
+			Intent intent = new Intent(this, DrawScreen.class);
+	    	intent.putExtra("mode", DrawScreen.DRAW_TO_SAVE);
+	    	intent.putExtra("domain", domain);
+	    	intent.putExtra("formName", formName);
+	    	intent.putExtra("useridField", useridField);
+	    	intent.putExtra("userid", userid);
+	    	intent.putExtra("passwdField", passwdField);
+	    	intent.putExtra("passwd", passwd);
+			
+			startActivityForResult(intent, DrawScreen.DRAW_TO_SAVE);
+		}
 	}
 }

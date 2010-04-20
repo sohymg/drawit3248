a6d10641c3142ab7a7dc3986e26ecba6b4ca6e40
fonglh <fonglh@0bbe4287-2352-73a6-adab-d028de2c3b49>
2010-04-20 04:35:09 +0000

    added functions to read from and write to the log file

--- src/com/drawIt/SelectDomain.java
+++ src/com/drawIt/SelectDomain.java
@@ -9,7 +9,6 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.AdapterView.OnItemClickListener;
 
